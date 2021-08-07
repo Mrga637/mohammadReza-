9b1b073a47cd79ca5cdc070200de7814484e3d92
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-07 21:20:47 +0430

    Create s2_h2_tashkhis baksh paziri.py

---
+++ s2_h2_tashkhis baksh paziri.py
@@ -0,0 +1,6 @@
+n1 = float(input('adade aval ra vared konin : '))
+n2 = float(input('adade dovomet ham begoo : '))
+if n1 % n2 == 0 :
+    print('adade aval shoma bar dovomin adadeton bakhsh pazir ast ')
+else :
+    print('adade avale shoma bar dovomi baksh pazir nist')
