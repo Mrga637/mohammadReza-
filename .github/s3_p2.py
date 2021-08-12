5427fea12efd41fef00d71fbb698d1e5d9b14677
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-12 11:42:18 +0430

    Update s3_p2.py

--- .github/s3_p2.py
+++ .github/s3_p2.py
@@ -1,5 +1,6 @@
-sen = input('sennet chande? ')
-def tanzim(sen= 13) :
+#sen = input('sennet chande? ')
+def tanzim() :
+    sen = int(input('sennet chande? '))
     if sen >13 :
         print('bia to ')
     else:
