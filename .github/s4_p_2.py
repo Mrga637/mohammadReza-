fba09ed67aad70d79f25ffb141d53ac7862f1ac2
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-26 12:26:48 +0430

    Update s4_p_2.py

--- .github/s4_p_2.py
+++ .github/s4_p_2.py
@@ -1,11 +1,14 @@
-Riazi1 = ['ali', 'mmd','reza','maryam','nader','reza']
-Fizik1 = ['ali','mmd', 'negar','fateme','mehrsa','nazi']
+Riazi1 = ('ali', 'mmd','reza','maryam','nader','reza')
+Fizik1 = ('ali','mmd', 'negar','fateme','mehrsa','nazi')
+my_set1 = set(Riazi1)
+my_set2 = set(Fizik1)
 kol1 = Fizik1+Riazi1
 tedad = len(kol1)
-my_set1 = set(Fizik1)
-my_set2 = set(Riazi1)
-eshterek = my_set1.intersection(my_set2)
+m = set(kol1)
+eshterek = my_set2.intersection(my_set1)
 
 print(f'ajzaye kol 2 kelas {kol1}ast va {tedad} nafar hatand')
 print(f'eshtrak 2 kelas {eshterek} hastand ke dar 2 kelas sherkat kardn')
+new = m - eshterek
+print(new)
 
