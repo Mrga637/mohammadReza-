76b2693a979e6ea49cb241e21733a0a474415928
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-26 12:03:24 +0430

    Update s4_p_2.py

--- .github/s4_p_2.py
+++ .github/s4_p_2.py
@@ -2,7 +2,10 @@ Riazi1 = ['ali', 'mmd','reza','maryam','nader','reza']
 Fizik1 = ['ali','mmd', 'negar','fateme','mehrsa','nazi']
 kol1 = Fizik1+Riazi1
 tedad = len(kol1)
-eshterek = Riazi1.intersection(Fizik1)
-kol = Riazi1.union(Fizik1)
+my_set1 = set(Fizik1)
+my_set2 = set(Riazi1)
+eshterek = my_set1.intersection(my_set2)
+
 print(f'ajzaye kol 2 kelas {kol1}ast va {tedad} nafar hatand')
 print(f'eshtrak 2 kelas {eshterek} hastand ke dar 2 kelas sherkat kardn')
+
