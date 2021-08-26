614049c0d75c6155f53b05c432fb36eaa8cdfff5
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-26 09:05:28 +0430

    Update s3_h2_S mosttil.py

--- .github/s3_h2_S mosttil.py
+++ .github/s3_h2_S mosttil.py
@@ -3,14 +3,17 @@ tool = float(input('tool ra vared konid : '))
 vahed = input('vahed ra vared konid : ')
 def masahat(arz, tool, vahed) :
     vahed1 = 'metr'
-
     result1 = arz * tool
     if vahed == 'mm':
         result = result1 * 10 ** -3
     elif vahed == 'cm':
         result = result1 * 10 ** -2
+    elif vahed == 'm':
+        result = result1
+    else :
+        result = result1
+        print('vaahed shoma esshtbeh ast va bsort pish faarz metr dar nazar grefte shod')
     print(f'masahat mostatil shoma ba vahed {vahed1} va masahat {result} mibashad')
 masahat(arz, tool, vahed)
-
 
 
