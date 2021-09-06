c1603153ec1ec21b9b4288411b03f37b5e9379cf
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-09-06 21:38:07 +0430

    Create s5_h1_add bozorg be kochik.py

---
+++ .github/s5_h1_add bozorg be kochik.py
@@ -0,0 +1,11 @@
+n1 = int(input('add begoo '))
+n2 = int(input('add begoo '))
+
+if n1 > n2 :
+    while n1 >=n2 :
+        print(n1)
+        n1-=1
+elif n1 < n2 :
+    while n1 <= n2 :
+        print(n2)
+        n2 -= 1
