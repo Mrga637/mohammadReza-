f1c961110d92aef7473f1bf124bba91663ad05fb
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-09-30 11:38:30 +0330

    Update s9_p1.py

--- .github/s9_p1.py
+++ .github/s9_p1.py
@@ -1,8 +1,11 @@
-f = open('C:\\Users\\Mohamadreza\\Documents\\test1py.txt','r')
-adad= []
-
-
-for i in f:
-    adad.append(i)
-sorted(adad)
-f.close()
+class Car():
+    def __init__(self, brand, model, motor_volume):
+        self.brand = brand
+        self.model = model
+        self.motor_volume = motor_volume
+    def boogh(self,boogh_lengh):
+        boogh_lengh= 'b'+ 'o'*boogh_lengh +'gh'
+        print(boogh_lengh)
+    def noor_bala(self):
+        print('noor bala')
+print(Car.boogh(5))
