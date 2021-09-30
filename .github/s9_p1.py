035911269b4fc339734a554fb26d50b2a9b74e55
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-09-30 12:11:35 +0330

    Update s9_p1.py

--- .github/s9_p1.py
+++ .github/s9_p1.py
@@ -1,11 +1,21 @@
 class Car():
-    def __init__(self, brand, model, motor_volume):
+    def __init__(self, brand, model, motor_volume, rang, curent_speed):
         self.brand = brand
         self.model = model
         self.motor_volume = motor_volume
+        self.rang =rang
+        self.curent_speed = curent_speed
+    def __str__(self):
+        return f'this is a {self.brand} {self.model} with {self.motor_volume} engine , with  {self.rang} colour'
     def boogh(self,boogh_lengh):
         boogh_lengh= 'b'+ 'o'*boogh_lengh +'gh'
         print(boogh_lengh)
     def noor_bala(self):
         print('noor bala')
-print(Car.boogh(5))
+    def gaz(self):
+        self.curent_speed += 20
+        if self.curent_speed == 120:
+           return 'bishtar nmire'
+
+mybmw = Car('bmw', 'm8',1.5,'black',0)
+mybmw.curent_speed()
