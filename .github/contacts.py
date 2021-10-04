8a8845c6153f8a3dc0e801b4135495b0671d3a71
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-10-04 23:25:26 +0330

    Update contacts.py

--- .github/contacts.py
+++ .github/contacts.py
@@ -5,39 +5,48 @@ import sqlite3
 def delete(wanted_last,wanted_name):
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
-    delet = f'DELETE from contacts where First_name =?  and Last_name = ?'
+    #delet = f'DELETE from contacts where First_name =?  and Last_name = ?'
     print('done')
-    cursor.execute(delet,  (wanted_last,wanted_name))
+    cursor.execute(f'DELETE from contacts where First_name =?  and Last_name = ?',  (wanted_last,wanted_name))
     conn.commit()
 
-print('''Hi welcome to my app this app let u to save your telefon numbers
-if you want to add sombody to your contacts write \'add\'or delete sombody write \'delete\'.
-Or you can search with 'search' by name and last name.\n''')
+print('''***Hi welcome to my app this app let u to save your telefon numbers
+if you want to add sombody to your contacts write \'add\'or delete sombudy write \'delete\'.
+Or you can search with 'search' by name and last name.***\n''')
+while 1==1 :
+   darkhast = input('What can i do for you? (add, delete, search) (: ').lower()
+   if darkhast == 'add':
+     conn = sqlite3.connect('data.db')
+     cursor = conn.cursor()
+     First_name = input('Name: ').lower()
+     Last_name = input('Last name: ').lower()
+     Telefon = int(input('Telefon number: '))
+     Time = datetime.datetime.now()
+   # queri_1 = f'insert into contacts values(?, ?,?,?)'
+     cursor.execute(f'insert into contacts values(?, ?,?,?)',   (First_name,Last_name,Time ,Telefon))
+     conn.commit()
+     cursor.close()
+     print('Done(:')
 
-darkhast = input('What can i do for you? (add, delete, search) (: ')
-if darkhast == 'add':
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    First_name = input('Name: ').lower()
-    Last_name = input('Last name: ').lower()
-    Telefon = int(input('Telefon number: '))
-    Time = datetime.datetime.now()
-    queri_1 = f'insert into contacts values(?, ?,?,?)'
-    cursor.execute(queri_1,   (First_name,Last_name,Time ,Telefon))
-    conn.commit()
-    cursor.close()
-elif darkhast == 'delete':
-   wanted_last = input('Write Lastname :) ')
-   wanted_name = input('Write Firstname :) ')
-   delete(wanted_name,wanted_last)
-elif darkhast == 'search':
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    wanted_n = input('tell me name')
-    wanted_f = input('Tell me lastname ?:) ')
-    query = f'select * from contacts where First_name = ? and Last_name = ? '
-    c.execute(query,  (wanted_n,wanted_f))
-    print()
-    conn.commit()
-    conn.close()
+   elif darkhast == 'delete':
+      wanted_last = input('Write Lastname :) ').lower()
+      wanted_name = input('Write Firstname :) ').lower()
+      delete(wanted_name,wanted_last)
+      print('Done(:')
+   elif darkhast == 'search':
+       conn = sqlite3.connect('data.db')
+       c = conn.cursor()
+       wanted_n = input('tell me name:').lower()
+       wanted_f = input('Tell me lastname ?:) ').lower()
+    #query = f'SELECT * from contacts where First_name=? and Last_name =? '
+       c.execute(f'SELECT * from contacts where First_name=? and Last_name =? ',  (wanted_n,wanted_f))
+       back = c.fetchall()
+       for ba in back:
+          print(ba)
+       conn.commit()
+       conn.close()
+       print('Done(:')
+   again = input('Do you want to do anything else?(yes\\no):').lower()
+   if again =='yes': pass
+   elif again == 'no' :break
 
